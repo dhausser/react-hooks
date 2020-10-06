e3c342d1252edcfda7383374b050528d159fb74e
Davy Hausser <davy.hausser@gmail.com>
2020-10-06 19:45:17 +0200

    :white_check_mark: useEffect: persistent state

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -5,14 +5,16 @@ import React from 'react'
 
 function Greeting({initialName = ''}) {
   const [name, setName] = React.useState(initialName)
+
   function handleChange(event) {
     setName(event.target.value)
   }
+
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
