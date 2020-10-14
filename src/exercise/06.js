82477372f6d1128171e81d793fa192c477173525
Davy Hausser <davy.hausser@gmail.com>
2020-10-14 14:54:44 +0200

    :white_check_mark: 06 - useEffect: HTTP requests started

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -6,10 +6,28 @@ import React from 'react'
 // fetchPokemon: the function we call to get the pokemon info
 // PokemonInfoFallback: the thing we show while we're loading the pokemon info
 // PokemonDataView: the stuff we use to display the pokemon info
-import {PokemonForm} from '../pokemon'
+import {
+  PokemonForm,
+  fetchPokemon,
+  PokemonInfoFallback,
+  PokemonDataView,
+} from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
   // 🐨 Have state for the pokemon (null)
+  const [pokemon, setPokemon] = React.useState(null)
+
+  React.useEffect(() => {
+    if (!pokemon || !pokemon.name) {
+      return
+    }
+    fetchPokemon('Pikachu').then(pokemonData => {
+      setPokemon(pokemonData)
+    })
+    const pokemonName = pokemon?.name
+    console.log(pokemonName)
+  }, [pokemon, pokemonName])
+
   // 🐨 use React.useEffect where the callback should be called whenever the
   // pokemon name changes.
   // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
